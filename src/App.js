afe588ab2a6aa221db1edef6e5ae4695d1de9eaa
pamodasilva99 <96543@fhss.sjp.ac.lk>
2022-07-07 16:28:16 +0530

    Create Navigation Bar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
+import Navbar from './root/frontend/Home/Navbar';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <h1> Hello </h1>      
-    </div>
+    <>
+      <Router>
+        <Navbar/>
+      </Router>     
+    </>
+    // <Switch>
+    // <Route path='/' exact/>
+    // </Switch>
   );
 }
 
