1ee575663116a6de1284a41a2eed3b8c3dc0036e
pamodasilva99 <96543@fhss.sjp.ac.lk>
2022-07-10 12:33:00 +0530

    Make Menu Page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Navbar from './root/frontend/components/Navbar';
 import Home from './root/frontend/Home/Home';
+import Menu from './root/frontend/Menu/Menu';
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
         <Navbar/>     
         <Routes>
           <Route path='/' element={<Home/>}/>
+          <Route path='/Menu' element={<Menu/>}/>
         </Routes>
       </Router>
     </>
