de375f7559a34cb1413c65738d93db6fac2dbd0e
pamodasilva99 <96543@fhss.sjp.ac.lk>
2022-07-10 21:29:18 +0530

    Create AboutUs Section

---
+++ src/root/frontend/AboutUs/team.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './team.css';
+import A1 from '../images/A1.jpg'
+
+function team() {
+  return (
+    <div className='teamMain'>
+      
+    </div>
+  )
+}
+
+export default team
