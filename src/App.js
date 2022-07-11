ce9ddfe28ef2639a13d900db5668fdb09f43aade
pamodasilva99 <96543@fhss.sjp.ac.lk>
2022-07-11 12:56:32 +0530

    Create Booking Page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navbar from './root/frontend/components/Navbar';
 import Home from './root/frontend/Home/Home';
 import Menu from './root/frontend/Menu/Menu';
 import AboutUs from './root/frontend/AboutUs/AboutUs';
+import Booking from './root/frontend/Booking/Booking';
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
           <Route path='/' element={<Home/>}/>
           <Route path='/Menu' element={<Menu/>}/>
           <Route path='/AboutUs' element={<AboutUs/>}/>
+          <Route path='/Booking' element={<Booking/>}/>
         </Routes>
       </Router>
     </>
---
+++ src/root/frontend/Booking/Booking.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import './Booking.css';
+import Footer from '../components/Footer';
+import book01 from '../images/book1.jpg'
+import book02 from '../images/book2.jpg'
+import { Button } from '../components/Button';
+
+function Booking() {
+
+  return (
+    <div className='main-booking'>
+      <div className='bookingContent'>
+        <div className='bookingImage'>
+          <img src={book01} alt="Booking-Pic01" className='Booking01'/>
+          <img src={book02} alt="Booking-Pic02" className='Booking02'/>
+        </div>
+        <div className='bookingForm'>
+          <h3> Book Your Table </h3>
+          <form className='Booking'>
+            <input type='text' placeholder='Full Name'/>
+            <input type='Email' placeholder='Your Email Address'/>
+            <input type='text' placeholder='Your Telephone Number'/>
+            <input type='text' placeholder='Date'/>
+            <input type='text' placeholder='Time'/>
+            <input type='text' placeholder='Number of Guest'/>
+              <Button className='btns' buttonStyle='btn--inline' buttonSize='btn--medium'>
+                  Book A Table
+              </Button>
+          </form>
+        </div>
+      </div>
+      <div className='Note'>
+        <p> *Note - </p>
+      </div>
+      <Footer/>
+    </div>
+  )
+}
+
+export default Booking
