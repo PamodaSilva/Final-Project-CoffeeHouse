606b2f86db2fd93d8de5805fc490863edfcd07f7
pamodasilva99 <96543@fhss.sjp.ac.lk>
2022-07-11 21:33:16 +0530

    Link menu and abotus pages with home page buttons

--- src/root/frontend/Home/HomeAbout/HomeAbout.js
+++ src/root/frontend/Home/HomeAbout/HomeAbout.js
@@ -2,8 +2,8 @@ import React from 'react'
 import Icon from '../../images/Icon.png';
 import CoffeeHouse from '../../images/CoffeeHouse.jpg';
 import HomeMenu from '../../images/HomeMenu.jpg';
+import { Link } from 'react-router-dom';
 import './HomeAbout.css';
-
 
 function HomeAbout() {
 
@@ -16,8 +16,9 @@ function HomeAbout() {
           <h1> Who we are </h1>
           <p> <b>Coffee House </b>is a place where you can get delicious high quality 
                   coffee drinks at affordable prices. </p>
-        <button type="button" className="custom__button" 
-        > More Details </button>
+        <Link to='/AboutUs'>
+          <button type="button" className="custom__button" > More Details </button>
+        </Link>
       </div>
 
       <div className="aboutUs-content_coffee">
@@ -27,7 +28,9 @@ function HomeAbout() {
       <div className="aboutUs-content_menu">
         <h1> Our Menu </h1>
         <p> A Cup Of Coffee Can Complete Your Day. </p>
-        <button type="button" className="custom__button"> See Our Menu </button>
+        <Link to='/Menu'>
+          <button type="button" className="custom__button"> See Our Menu </button>
+        </Link>
         <img src= {HomeMenu} alt='Coffee House' className='image2'/>
       </div>
     </div>
