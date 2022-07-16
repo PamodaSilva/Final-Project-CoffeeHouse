ebf6b1dddc4d479d2212fb72a55c8680dc171eee
pamodasilva99 <96543@fhss.sjp.ac.lk>
2022-07-16 16:53:00 +0530

    Create Banckend Main File

--- Backend/server.js
+++ Backend/server.js
@@ -1,2 +1,22 @@
-//  mongodb+srv://pamodasilva:<Pamodasilva99>@cluster0.qq786.mongodb.net/?retryWrites=true&w=majority
- 
+
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+const bookRoutes = require("./routes/Booking");
+app.use(bookRoutes);
+
+const PORT = 8080;
+const URL = "mongodb+srv://pamodasilva:<Pamodasilva99>@cluster0.qq786.mongodb.net/?retryWrites=true&w=majority"
+mongoose.connect(URL)
+    .then(() => {
+        console.log("Connect")
+    })
+    .catch ((err) => console.log("DB error",err))
+app.listen(PORT, () => {
+    console.log("app is runninsdfg on")
+})
