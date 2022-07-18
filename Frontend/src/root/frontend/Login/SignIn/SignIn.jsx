bae4752488a4994e739befd4b10887e07f2661be
pamodasilva99 <96543@fhss.sjp.ac.lk>
2022-07-18 15:53:16 +0530

    Add Alert on Login Page

--- Frontend/src/root/frontend/Login/SignIn/SignIn.jsx
+++ Frontend/src/root/frontend/Login/SignIn/SignIn.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './SignIn.css'
 import { Link } from 'react-router-dom';
 import Footer from '../../components/Footer';
 
 function SignIn() {
+
+  const [Email, setEmail] = useState("");
+  const [Password, setPassword] = useState("");
+
+  const Login = () => {
+    if (!Email) {
+      alert("Email Required");
+    }
+    if (!Password) {
+      alert("Password Required");
+    }
+  };
+
   return (
     <>
     <div className='signIn'>
@@ -11,10 +24,32 @@ function SignIn() {
       <h2 className="signInTitle"> Sign In </h2>
       <form className='signInForm'>
           <label>Email</label>
-          <input type="text" className='signInInput' placeholder="Enter your email" />
+          <input 
+            type="text" 
+            className='signInInput' 
+            placeholder="Enter your email" 
+            value={Email}
+              onChange={(e) => {
+                setEmail(e.target.value);
+              }}/>
+
           <label>Password</label>
-          <input type="password" className='signInInput' placeholder="Enter your password" />
-          <button className='signInButton'>Login</button>
+          <input 
+            type="password" 
+            className='signInInput' 
+            placeholder="Enter your password" 
+            value={Password}
+              onChange={(e) => {
+                setPassword(e.target.value);
+              }}/>
+
+          <button 
+          className='signInButton'
+          onClick={(e) => {
+            e.preventDefault();
+            Login();
+          }}>  Login    </button>
+
           <label> Don't have an Account ? </label>
           <Link to='/sign-up'>
             <button className='signInRegisterButton'>Register</button>
